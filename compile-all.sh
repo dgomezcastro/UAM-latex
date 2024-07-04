6cffbdc47b2c9f73570c0bb229907a52d274ee4a
David <david@gomezcastro.xyz>
2024-07-04 09:07:57 +0200

    ignore sesion pdfs

--- compile-all.sh
+++ compile-all.sh
@@ -5,16 +5,17 @@ do
     cd 0$i*
     pdflatex -shell-escape sesion.tex
     pdflatex -shell-escape sesion.tex
-    rm *.aux
-    rm *.log
-    rm *.nav
-    rm *.out
-    rm *.snm
-    rm *.toc
-    rm *.vrb
-    rm *.fdb_latexmk
-    rm *.synctex.gz
-    rm *.fls
-    rm -R _minted-*
     cd ..
 done
+
+rm ./**/*.aux
+rm ./**/*.log
+rm ./**/*.nav
+rm ./**/*.out
+rm ./**/*.snm
+rm ./**/*.toc
+rm ./**/*.vrb
+rm ./**/*.fdb_latexmk
+rm ./**/*.synctex.gz
+rm ./**/*.fls
+rm -R ./**/_minted-*
