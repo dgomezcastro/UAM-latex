f3ca33e6f02564128cd8fa59c1b4311dd7516870
David <david@gomezcastro.xyz>
2024-07-04 09:52:22 +0200

    added clean-up.sh

---
+++ clean-up.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm ./**/*.aux
+rm ./**/*.log
+rm ./**/*.nav
+rm ./**/*.out
+rm ./**/*.snm
+rm ./**/*.toc
+rm ./**/*.vrb
+rm ./**/*.fdb_latexmk
+rm ./**/*.synctex.gz
+rm ./**/*.fls
+rm -R ./**/_minted-*
