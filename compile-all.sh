b33b10a7699d5e949cab14a810dbb3bb25122e2c
David <david@gomezcastro.xyz>
2024-08-16 19:32:48 +0200

    arreglado compile-all

--- compile-all.sh
+++ compile-all.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-for i in {1..10}
+for i in {1..9}
 do
     cd 0$i*
     pdflatex -shell-escape sesion.tex
     pdflatex -shell-escape sesion.tex
     cd ..
 done
+
+cd 10*
+pdflatex -shell-escape sesion.tex
+pdflatex -shell-escape sesion.tex
+cd ..
 
 ./clean-up.sh
