3aa75b2d50c2647427f085c416dc899f767b2cd1
David <david@gomezcastro.xyz>
2024-08-18 16:26:55 +0200

    formato de pagina y referencias cruzadas separado del resto

--- compile-all.sh
+++ compile-all.sh
@@ -2,15 +2,20 @@
 
 for i in {1..9}
 do
+    echo $i
     cd 0$i*
-    pdflatex -shell-escape sesion.tex
-    pdflatex -shell-escape sesion.tex
+    pdflatex -shell-escape -interaction=batchmode sesion.tex
+    pdflatex -shell-escape -interaction=batchmode sesion.tex
     cd ..
 done
 
-cd 10*
-pdflatex -shell-escape sesion.tex
-pdflatex -shell-escape sesion.tex
-cd ..
+for i in {10..12}
+do
+    echo $i
+    cd $i*
+    pdflatex -shell-escape -interaction=batchmode sesion.tex
+    pdflatex -shell-escape -interaction=batchmode sesion.tex
+    cd ..
+done 
 
 ./clean-up.sh
