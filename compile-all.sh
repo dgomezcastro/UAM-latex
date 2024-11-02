88ad5dea76f732b4ef311ffcfc226d3e7247ad62
David <david@gomezcastro.xyz>
2024-11-02 18:09:44 +0100

    create curso-latex.tex

--- compile-all.sh
+++ compile-all.sh
@@ -4,8 +4,10 @@ for i in {1..9}
 do
     echo $i
     cd 0$i*
-    pdflatex -shell-escape -interaction=batchmode sesion.tex
-    pdflatex -shell-escape -interaction=batchmode sesion.tex
+    pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
+    pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
+    biber sesion-0$i 
+    pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
     cd ..
 done
 
@@ -13,8 +15,10 @@ for i in {10..12}
 do
     echo $i
     cd $i*
-    pdflatex -shell-escape -interaction=batchmode sesion.tex
-    pdflatex -shell-escape -interaction=batchmode sesion.tex
+    pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
+    pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
+    biber sesion-$i 
+    pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
     cd ..
 done 
 
