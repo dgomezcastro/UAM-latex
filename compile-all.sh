94de64babcdd35a32d272e21a6d97ffb58a004fa
David Gomez-Castro <david@gomezcastro.xyz>
2025-10-31 14:59:52 +0100

    actualizada compilacion para biber

--- compile-all.sh
+++ compile-all.sh
@@ -5,8 +5,8 @@ do
     echo $i
     cd 0$i*
     pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
-    pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
     biber --input-directory ../tex sesion-0$i 
+    pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
     pdflatex -shell-escape -interaction=batchmode sesion-0$i.tex
     cd ..
 done
@@ -16,15 +16,15 @@ do
     echo $i
     cd $i*
     pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
-    pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
     biber --input-directory ../tex sesion-$i 
+    pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
     pdflatex -shell-escape -interaction=batchmode sesion-$i.tex
     cd ..
 done
 
 pdflatex -shell-escape -interaction=batchmode curso-latex.tex
-pdflatex -shell-escape -interaction=batchmode curso-latex.tex
 biber --input-directory tex curso-latex
+pdflatex -shell-escape -interaction=batchmode curso-latex.tex
 pdflatex -shell-escape -interaction=batchmode curso-latex.tex
 
 ./clean-up.sh
